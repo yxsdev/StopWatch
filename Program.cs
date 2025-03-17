e47b0456364036d18ff213c5845b711a6629ec18
Yasmin Dantas <yasmindantasbusiness@gmail.com>
2025-03-17 12:25:31 -0300

    feat: Finalizado o menu principal do cronômetro.

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Net.NetworkInformation;
 using System.Threading;
 
 namespace StopWatch
@@ -7,12 +9,38 @@ namespace StopWatch
     {
         static void Main(string[] args)
         {
-            Start();
+            Menu();
         }
 
-        static void Start()
+        static void Menu()
         {
-            int time = 10;
+            Console.Clear();
+            Console.WriteLine("S = Segundo => 10s = 10 segundos");
+            Console.WriteLine("M = Minuto => 1m = 1 minuto");
+            Console.WriteLine("0 = Sair da aplicação");
+            Console.WriteLine("Use ctrl + c para interromper o cronômetro.");
+            Console.WriteLine("Quanto tempo deseja contar?");
+
+            string data = Console.ReadLine().ToLower();
+            char type = char.Parse(data.Substring(data.Length - 1, 1));
+            int time = int.Parse(data.Substring(0, data.Length - 1));
+            int multiplier = 1;
+
+            if (type == 'm')
+            {
+                multiplier = 60;
+            }
+
+            if (time == 0)
+            {
+                System.Environment.Exit(0);
+            }
+
+            Start(time * multiplier);
+        }
+
+        static void Start(int time = 0)
+        {
             int currentTime = 0;
 
             while (currentTime != time)
@@ -22,6 +50,11 @@ namespace StopWatch
                 Console.WriteLine(currentTime);
                 Thread.Sleep(1000);
             }
+
+            Console.Clear();
+            Console.WriteLine("StopWatch Finalizado.");
+            Thread.Sleep(2500);
+            Menu();
         }
     }
 }
